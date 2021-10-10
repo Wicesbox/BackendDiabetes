722fd074781db5c96371b54b5030fa05ed3dd17f
Wicesbox <wicesbox@gmail.com>
2021-10-10 13:51:59 +0700

    first commit

---
+++ old_main (2).py
@@ -0,0 +1,79 @@
+from typing import Optional
+from fastapi import FastAPI, HTTPException, Depends, Request,status
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from hashing import Hash
+from jwttoken import create_access_token
+from oauth import get_current_user
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+from pymongo import MongoClient
+mongodb_uri = 'mongodb+srv://dbUser:chula123@cluster0.nds32.mongodb.net/myFirstDatabase?ssl=true&ssl_cert_reqs=CERT_NONE'
+port = 8000
+client = MongoClient(mongodb_uri, port)
+db = client["User"]
+
+class User(BaseModel):
+    username: str
+    real_name: str
+    surname: str
+    dob: str
+    gender: str
+    weight: int
+    height: int
+    tel: str
+    email: str
+    password: str
+    confirmpassword: str
+
+class Login(BaseModel):
+	username: str
+	password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+@app.get("/")
+def read_root(current_user:User = Depends(get_current_user)):
+	return {"data":"Hello World"}
+
+@app.post('/register')
+def create_user(request:User):
+	hashed_pass = Hash.bcrypt(request.password)
+	user_object = dict(request)
+	user_object["password"] = hashed_pass
+	user_id = db["users"].insert(user_object)
+	# print(user)
+	return {"res":"created"}
+
+# 
+@app.post('/login')
+def login(request:OAuth2PasswordRequestForm = Depends()):
+	print("request: ", request)
+	user = db["users"].find_one({"username":request.username})
+	if not user:
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail = f'No user found with this {request.username} username')
+	if not Hash.verify(user["password"],request.password):
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail = f'Wrong Username or password')
+	access_token = create_access_token(data={"sub": user["username"] })
+	return {"access_token": access_token, "token_type": "bearer"}
